3616e740dc022233b74b7bb332e8402df320671e
Blezz-tech <72557631+Blezz-tech@users.noreply.github.com>
2022-10-23 17:01:11 +0300

    2

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,10 +5,9 @@ const userRouter = require('./userRouter');
 const brandRouter = require('./brandRouter');
 const typeRouter = require('./typeRouter');
 
-
-router.use('./user', userRouter);
-router.use('./type', typeRouter);
-router.use('./brand', brandRouter);
-router.use('./device', deviceRouter);
+router.use('/user', userRouter);
+router.use('/type', typeRouter);
+router.use('/brand', brandRouter);
+router.use('/device', deviceRouter);
 
 module.exports = router;
