d59139c56ed6d1ab3de7b75904e186797d366f80
Blezz-tech <markus.jenya04@gmail.com>
2023-03-30 12:22:03 +0300

    update

---
+++ php/chapter-1/lesson-1.php
@@ -0,0 +1,8 @@
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+
+include $root."/html-components/head.htm";
+include $root."/html-components/header.htm";
+include $root."/html-components/chapters/chapter-1/lesson-1.htm";
+include $root."/html-components/footer.htm";
+?>
