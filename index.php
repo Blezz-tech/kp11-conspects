b8c7f3f73221bfe5be87274009984ac44d36ead8
Blezz-tech <72557631+Blezz-tech@users.noreply.github.com>
2023-04-02 12:47:37 +0300

    update

--- index.php
+++ index.php
@@ -1,7 +1,57 @@
 <?php
 $root = $_SERVER['DOCUMENT_ROOT'];
 
+$GLOBALS['g_chapters'] = [
+    1 => 'Глава 1 Введение',
+    2 => 'Глава 2 HTML',
+    3 => 'Глава 3 CSS',
+    4 => 'Глава 4 Другое'
+];
 
+$GLOBALS['g_lessons'] = [
+    1 => [
+        1 => 'Урок 1 Что такое вёрстка-создание сайтов?',
+        2 => 'Урок 2 Подготвка к работе-созданию сайта',
+        3 => 'Урок 3 Настройка структуы',
+        4 => 'Урок 4 Лучшие браузеры для работы с сайтом'
+    ],
+    2 => [
+        1 => 'Урок 1 Основаня разметка и её свойства',
+        2 => 'Урок 2 Правильная разбивка сайта на блоки и составляюшие',
+        3 => 'Урок 3 Основные теги для работы',
+        4 => 'Урок 4 Правильная структура HTML документа',
+    ],
+    3 => [
+        1 => 'Урок 1 Подговительные работы CSS',
+        2 => 'Урок 2 наследование и иерархия классов',
+        3 => 'Урок 3 основные свойства CSS для работы',
+        4 => 'Урок 4 Медиа запросы и адаптивность',
+    ],
+    4 => [
+        1 => 'Доп источники',
+        2 => 'Советы'
+    ]
+];
+
+function generatorNav() {
+    $g_chapters = $GLOBALS['g_chapters'];
+    $g_lessons = $GLOBALS['g_lessons'];
+
+    $nav = '<nav class="nav">>';
+    foreach($g_lessons as $i => $lessons) {
+        $nav .= '<div class="chapter">'.
+        '<input id="toggle1" type="radio" class="chapter-toggle" name="toggle" checked />'.
+        '<label for="toggle1">'.$g_chapters[$i].'</label>'.
+        '<section>';
+        foreach($lessons as $j => $lesson) {
+            $nav .= '<p class="lesson"><a href="'.'/lesson?chapter='.$i.'&lesson='.$j.'">'.$g_lessons[$i][$j].'</a></p>';
+        }
+        $nav .= '</section>'.
+        '</div>';
+    }
+    $nav .= '</nav>';
+    return $nav;
+}
 
 function getRequestPath() {
     $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -21,85 +71,19 @@ function getMethod($routes, $path) {
 
 $routes = [];
 
-function lesson() {
-    $root = $_SERVER['DOCUMENT_ROOT'];
-
-    $arr = [];
-    $arrPaths = [];
-    $string = '';
-    
-    $chapterPath = "/components/learn";
-    $chapterArr = scandir($root.$chapterPath);
-    unset($chapterArr[0]);
-    unset($chapterArr[1]);
-
-    foreach($chapterArr as $i => $chapterName) {
-        $lessonPath = $chapterPath.'/'.$chapterName;
-        $lessonArr = scandir($root.$lessonPath);
-        unset($lessonArr[0]);
-        unset($lessonArr[1]);
-
-        $arrPaths[$chapterName] = [];
-
-        foreach($lessonArr as $j => $lessonName) {
-            array_push($arrPaths[$chapterName], substr($lessonName, 0, strpos($lessonName, '.html')));
-        }
-    }
-
-    $nav = '<nav>';
-    $i = 0;
-    foreach($arrPaths as $chapter => $lessons) {
-        $i += 1;
-        $nav .= '<div class="chapter">'.
-        '<input id="toggle1" type="radio" class="chapter-toggle" name="toggle" checked />'.
-        '<label for="toggle1">'.$chapter.'</label>'.
-        '<section>';
-        foreach($lessons as $j => $lesson) {
-            $nav .= '<p class="lesson"><a href="'.'/chapter-'.$i.'/lesson-'.$j.'">'.$lesson.'</a></p>';
-        }
-        $nav .= '</section>'.
-        '</div>';
-    }
-    $nav .= '</nav>';
-
-    $i = 0;
-    foreach($arrPaths as $chapter => $lessons) {
-        $i += 1;
-        // echo $chapterPath.'/'.$chapter."<br>";
-
-        foreach($lessons as $j => $lesson) {
-            $routePath = $chapter.'/'.$lesson;
-            // echo $chapterPath.'/'.$routePath."<br>";
-            $arr['/chapter-'.$i.'/lesson-'.$j] = function () {
-                return 
-                file_get_contents($root."/components/meta/_head.html").
-                file_get_contents($root.$chapterPath.'/'.$routePath.'.html').
-                $nav.
-                file_get_contents($root."/components/meta/_foot.html");
-            };
-        }
-    }
-    print_r($arr);
-    return $arr;
-}
-
-$routes = lesson(); 
 
 $routes['/lesson'] = function () {
     $root = $_SERVER['DOCUMENT_ROOT'];
 
-    // номер главы и номер урока
-    $pages = [
-        33 => 'Сага о хомячках',
-        54 => 'Мыши в тумане'
-    ];
+    if (isset($_GET['chapter']) && isset($_GET['lesson'])) {
+        $nav = generatorNav();
 
-    if (isset($_GET['id']) && isset($pages[$_GET['id']])) {
-        return $pages[$_GET['id']];
+        return 
+        file_get_contents($root."/components/meta/_head.html").
+        file_get_contents($root."/components/meta/_foot.html");
+
     }
-
     return notFound();
-
 };
 
 $routes['/'] = function () {
@@ -130,13 +114,12 @@ function notFound() {
 
 
 $path = getRequestPath();
-echo '<br>'.'<br>'.$path;
-
 $method = getMethod($routes, $path);
 echo $method();
 
 
 
+echo '<br>'.'<br>'.$path;
 
 
 ?>
