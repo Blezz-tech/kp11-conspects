fb10ab570eebeecb58f27adff838c90bdf8458a0
Blezz-tech <markus.jenya04@gmail.com>
2023-05-20 07:47:53 +0300

    init

---
+++ store/products/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Мерч сталкера',
+    }
+    return render(request, "products/index.html", context)
+
+def products(request):
+    context = {
+        'title': 'Каталог мерча сталкера',
+        'products': [
+            {
+                'name': 'Лунный свет',
+                'image': '/static/img/artefacts/artefatc-1.png',
+                'price': 6000,
+                'description': 'Описание артефакта Лунный свет'
+            },
+            {
+                'name': 'Лунный свет',
+                'image': '/static/img/artefacts/artefatc-1.png',
+                'price': 6000,
+                'description': 'Описание артефакта Лунный свет'
+            },
+            {
+                'name': 'Лунный свет',
+                'image': '/static/img/artefacts/artefatc-1.png',
+                'price': 6000,
+                'description': 'Описание артефакта Лунный свет'
+            },
+            {
+                'name': 'Лунный свет',
+                'image': '/static/img/artefacts/artefatc-1.png',
+                'price': 6000,
+                'description': 'Описание артефакта Лунный свет'
+            },
+            {
+                'name': 'Лунный свет',
+                'image': '/static/img/artefacts/artefatc-1.png',
+                'price': 6000,
+                'description': 'Описание артефакта Лунный свет'
+            },
+        ]
+    }
+    return render(request, "products/products.html", context)
