47fa4c4fbed52a4550c8c7359a1869427ce29665
Blezz-tech <markus.jenya04@gmail.com>
2023-10-12 22:28:27 +0300

    update

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+$arr = json_decode(file_get_contents("./var/course.json"), true);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+  <h1 class="text-center">Курсы</h1>
+  <h2 class="text-center">Форма</h2>
+  <div class="container">
+    <form action="" method="post">
+      <div class="mb-3">
+        <input type="text" class="form-control" name="name" placeholder="ФИО">
+      </div>
+      <div class="mb-3">
+        <input type="text" class="form-control" name="coure" placeholder="Какие курсы вас заинтересовали?">
+      </div>
+      <div class="mb-3">
+        <input type="text" class="form-control" name="phone" placeholder="+7 (999) 999-99-99">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+  <h2 class="text-center">Таблица</h2>
+  <div class="container">
+    <?php foreach ($arr as $key => $value) : ?>
+      <div class="mb-5">
+        <h4 class="text-center mb-3"><?= $key ?></h4>
+        <table class="table table-striped">
+          <tr class="row">
+            <th scope="col" class="col-5">Название</th>
+            <th scope="col" class="col-4">Длительность курса</th>
+            <th scope="col" class="col-2">Стоимость обучения</th>
+            <th scope="col" class="col-1">Возраст</th>
+          </tr>
+          <?php foreach ($value as $course) : ?>
+            <tr class="row">
+              <td scope="col" class="col-5"><?= $course["Название"] ?></td>
+              <td scope="col" class="col-4"><?= ($course["Длительность курса"] == "" ? "" : $course["Длительность курса"] . " часов") ?></td>
+              <td scope="col" class="col-2"><?= ($course["Стоимость обучения"] == "" ? "" : $course["Стоимость обучения"] . " руб") ?></td>
+              <td scope="col" class="col-1"><?= $course["Возраст"] ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </table>
+      </div>
+    <?php endforeach; ?>
+  </div>
+  <h2 class="text-center">Контакты</h2>
+  <div class="container">
+    <p>+7(499)1-504-504</p>
+    <p>priem@kp11.ru</p>
+    <p>Москва, Ленинградское шоссе, д.13А</p>
+    <a href="https://vk.com/gapoukp11" target="_blank" rel="nofollow noopener" style="width: 30px; height: 30px;"><svg class="t-sociallinks__svg" width="30px" height="30px" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M50 100c27.614 0 50-22.386 50-50S77.614 0 50 0 0 22.386 0 50s22.386 50 50 50ZM25 34c.406 19.488 10.15 31.2 27.233 31.2h.968V54.05c6.278.625 11.024 5.216 12.93 11.15H75c-2.436-8.87-8.838-13.773-12.836-15.647C66.162 47.242 71.783 41.62 73.126 34h-8.058c-1.749 6.184-6.932 11.805-11.867 12.336V34h-8.057v21.611C40.147 54.362 33.838 48.304 33.556 34H25Z" fill="#000000"></path>
+      </svg></a>
+    <a href="https://t.me/joinchat/S1XweP3xiGDN4PvT" target="_blank" rel="nofollow noopener" style="width: 30px; height: 30px;"><svg class="t-sociallinks__svg" width="30px" height="30px" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M50 100c27.614 0 50-22.386 50-50S77.614 0 50 0 0 22.386 0 50s22.386 50 50 50Zm21.977-68.056c.386-4.38-4.24-2.576-4.24-2.576-3.415 1.414-6.937 2.85-10.497 4.302-11.04 4.503-22.444 9.155-32.159 13.734-5.268 1.932-2.184 3.864-2.184 3.864l8.351 2.577c3.855 1.16 5.91-.129 5.91-.129l17.988-12.238c6.424-4.38 4.882-.773 3.34.773l-13.49 12.882c-2.056 1.804-1.028 3.35-.129 4.123 2.55 2.249 8.82 6.364 11.557 8.16.712.467 1.185.778 1.292.858.642.515 4.111 2.834 6.424 2.319 2.313-.516 2.57-3.479 2.57-3.479l3.083-20.226c.462-3.511.993-6.886 1.417-9.582.4-2.546.705-4.485.767-5.362Z" fill="#000000"></path>
+      </svg></a>
+  </div>
+
+  <script src="./bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
