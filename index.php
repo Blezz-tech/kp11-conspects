4fbd359353b0da453b74dff77a1d20e12e2816f8
Blezz-tech <markus.jenya04@gmail.com>
2023-10-23 21:27:59 +0300

    update

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 $tables = json_decode(file_get_contents("./var/course.json"), true);
-
 $studnets = [1, 2, 3, 4, 5, 6];
+
 ?>
 
 <!DOCTYPE html>
@@ -16,8 +16,8 @@ $studnets = [1, 2, 3, 4, 5, 6];
 </head>
 
 <body>
-  <h1 class="text-center">Курсы</h1>
-  <h2 class="text-center">Форма</h2>
+  <h1 class="mt-3 text-center">Курсы</h1>
+  <h2 class="mt-3 text-center">Форма</h2>
   <div class="container">
     <form action="" method="post">
       <div class="mb-3">
@@ -32,7 +32,7 @@ $studnets = [1, 2, 3, 4, 5, 6];
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
   </div>
-  <h2 class="text-center">Таблица</h2>
+  <h2 class="mt-3 text-center">Таблица</h2>
   <div class="container">
     <?php foreach ($tables as $key => $value) : ?>
       <div class="mb-5">
@@ -56,7 +56,7 @@ $studnets = [1, 2, 3, 4, 5, 6];
       </div>
     <?php endforeach; ?>
   </div>
-  <h2 class="text-center">Контакты</h2>
+  <h2 class="mt-3 text-center">Контакты</h2>
   <div class="container">
     <p>+7(499)1-504-504</p>
     <p>priem@kp11.ru</p>
@@ -68,7 +68,7 @@ $studnets = [1, 2, 3, 4, 5, 6];
         <path fill-rule="evenodd" clip-rule="evenodd" d="M50 100c27.614 0 50-22.386 50-50S77.614 0 50 0 0 22.386 0 50s22.386 50 50 50Zm21.977-68.056c.386-4.38-4.24-2.576-4.24-2.576-3.415 1.414-6.937 2.85-10.497 4.302-11.04 4.503-22.444 9.155-32.159 13.734-5.268 1.932-2.184 3.864-2.184 3.864l8.351 2.577c3.855 1.16 5.91-.129 5.91-.129l17.988-12.238c6.424-4.38 4.882-.773 3.34.773l-13.49 12.882c-2.056 1.804-1.028 3.35-.129 4.123 2.55 2.249 8.82 6.364 11.557 8.16.712.467 1.185.778 1.292.858.642.515 4.111 2.834 6.424 2.319 2.313-.516 2.57-3.479 2.57-3.479l3.083-20.226c.462-3.511.993-6.886 1.417-9.582.4-2.546.705-4.485.767-5.362Z" fill="#000000"></path>
       </svg></a>
   </div>
-  <h2 class="text-center">Фотокарточки с занятий</h2>
+  <h2 class="mt-3 text-center">Фотокарточки с занятий</h2>
   <div class="container">
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner">
@@ -88,7 +88,21 @@ $studnets = [1, 2, 3, 4, 5, 6];
       </button>
     </div>
   </div>
-
+  <h2 class="mt-3 text-center">Информация для абитуриента</h2>
+  <div class="container">
+    <div class="list-group">
+      <a target="_blank" href="./assets/documents/Музыкальное звукооператорское мастерство программа.pdf" class="list-group-item list-group-item-action">Вступительные испытания "Музыкальное звукооператорское мастерство"</a>
+      <a target="_blank" href="./assets/documents/anim_compressed.pdf" class="list-group-item list-group-item-action">Вступительные испытания "Анимация и анимационное кино (по видам)"</a>
+      <a target="_blank" href="./assets/documents/Театральная и аудиовизуальная техника программа.pdf" class="list-group-item list-group-item-action">Вступительные испытания "Театральная и аудиовизуальная техника"</a>
+      <a target="_blank" href="./assets/documents/54.02.08 Техника и искусство фотографии.pdf" class="list-group-item list-group-item-action">Вступительные испытания "Техника и искусство фотографии"</a>
+      <a target="_blank" href="https://kp11.mskobr.ru/postuplenie-v-kolledzh/priemnaya-komissiya" class="list-group-item list-group-item-action">Документы при поступлении</a>
+      <a target="_blank" href="./assets/documents/Сводка ежедневная 22.08.pdf" class="list-group-item list-group-item-action">Количество поданных заявлений</a>
+      <a target="_blank" href="https://rutube.ru/video/d7b89293659d939acaa3054784aeee23/" class="list-group-item list-group-item-action">Как подать документы на mos.ru</a>
+      <a target="_blank" href="https://kp11.mskobr.ru/postuplenie-v-kolledzh/priemnaya-komissiya" class="list-group-item list-group-item-action">Предварительный медицинский осмотр</a>
+      <a target="_blank" href="https://kp11.mskobr.ru/postuplenie-v-kolledzh/priemnaya-komissiya" class="list-group-item list-group-item-action">Документы приемной комиссии</a>
+      <a target="_blank" href="https://kp11.mskobr.ru/postuplenie-v-kolledzh/specialnosti-professii" class="list-group-item list-group-item-action">Контрольные цифры приема</a>
+    </div>
+  </div>
   <script src="./bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
