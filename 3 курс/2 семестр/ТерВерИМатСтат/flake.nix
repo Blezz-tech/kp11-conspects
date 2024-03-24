4e1ba03a57a51d7f4afa01a480a13436f1e4a09a
Blezz-tech <72557631+Blezz-tech@users.noreply.github.com>
2024-03-24 21:31:29 +0300

    update

--- 3 курс/2 семестр/ТерВерИМатСтат/flake.nix
+++ 3 курс/2 семестр/ТерВерИМатСтат/flake.nix
@@ -19,6 +19,8 @@
           python311Packages.jupyter
           python311Packages.matplotlib
           python311Packages.numpy
+          python311Packages.scipy
+          python311Packages.nbconvert
         ];
         buildInputs = with pkgs; [];
       };
