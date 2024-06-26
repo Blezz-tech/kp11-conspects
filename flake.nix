73fb1b2ddebb9ee5d221a49a00cc4d7d9e5285ba
blezz-tech <markus.jenya04@yandex.ru>
2024-06-26 17:03:58 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv/v0.6.3";
+  };
+
+  outputs = { nixpkgs, devenv, systems, ... }@inputs:
+    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      devShells = forEachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [{
+
+              packages = with pkgs; [
+                mysql-workbench
+              ];
+
+              languages.php = {
+                enable = true;
+                version = "8.1";
+              };
+
+              services.mysql = {
+                enable = true;
+                package = pkgs.mariadb;
+                initialDatabases = [{ name = "qual-pei"; }];
+                ensureUsers = [
+                  {
+                    name = "root";
+                    password = "";
+                    ensurePermissions = { "root.*" = "ALL PRIVILEGES"; };
+                  }
+                ];
+                settings = {
+                  mysqld = {
+                    "bind_address" = "localhost";
+                  };
+                };
+              };
+
+              scripts = {
+                EnvClearAll.exec = "rm -rf ./.devenv ./.direnv";
+                EnvClearStatic.exec = "rm -rf ./.devenv/state";
+              };
+            }];
+          };
+        });
+    };
+}
