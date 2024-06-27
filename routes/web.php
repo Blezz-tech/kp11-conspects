d95e9dfe3a4b514772977bc4dc9fed9708c3eb5b
blezz-tech <markus.jenya04@yandex.ru>
2024-06-27 12:31:54 +0300

    добавлена заготовка маршрутов

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::middleware(['guest'])->group(function () {
+    // Route::get('/register', [RegisterController::class, 'regform'])->name('regform');
+    // Route::post('/register', [RegisterController::class, 'register'])->name('register');
+    // Route::get('/login', [RegisterController::class, 'loginform'])->name('loginform');
+    // Route::post('/login', [RegisterController::class, 'login'])->name('login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    // Route::get('/mytickets', [TicketController::class, 'mytickets'])->name('mytickets');
+    // Route::get('/createticket', [TicketController::class, 'ShowTicketForm'])->name('createticket');
+    // Route::post('/createticket', [TicketController::class, 'store'])->name('storeticket');
+    // Route::get('/logout', [RegisterController::class, 'logout'])->name('logout');
+});
+
+Route::middleware(['admin'])->group(function () {
+    // Route::get('/admin', [AdminController::class, 'showPanel'])->name('showpanel');
+    // Route::post('/changestatus', [TicketController::class, 'changestatus'])->name('changestatus');
 });
