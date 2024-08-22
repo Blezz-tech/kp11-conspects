427e1baf475a637865ca96593ba8126d03dddd8a
Blezz-tech <markus.jenya04@yandex.ru>
2024-08-22 11:13:13 +0300

    Добавление md

---
+++ src/components/MarkdownRenderer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-html="markdown.render(source)" />
+</template>
+
+<script setup lang="ts">
+import MarkdownIt from "markdown-it";
+import markdownItClass from "markdown-it-class";
+
+const markdown = new MarkdownIt()
+    .use(markdownItClass, {
+        h4: ['q-mt-lg', 'q-mb-lg'],
+        ul: ['q-mt-lg'],
+        ol: ['q-mt-sm', 'q-mb-sm'],
+        li: ['disc'],
+        hr: ['q-mt-lg', 'q-mb-lg'],
+    });
+
+defineProps({
+    source: {
+        type: String,
+        default: ""
+    }
+});
+</script>
