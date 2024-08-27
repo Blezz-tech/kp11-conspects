c807dc8d42ee3d730549bbf5a8a49811ba4ca6d4
blezz-tech <markus.jenya04@yandex.ru>
2024-08-27 22:35:22 +0300

    000001 / Добавлены данные для сидера

--- backend-v1/database/seeders/ArticleSeeder.php
+++ backend-v1/database/seeders/ArticleSeeder.php
@@ -15,4 +15,70 @@ class ArticleSeeder extends Seeder
     {
         Article::factory()->count(10)->create();
     }
+
+    private function articles(): array
+    {
+        return [
+            [
+                'title' => 'ген',
+                'subtitle' => 'подряд',
+                'path_text' => null,
+                'img' => '/src/assets/1.png'
+            ],
+            [
+                'title' => 'стена',
+                'subtitle' => 'в грунте',
+                'path_text' => null,
+                'img' => '/src/assets/2.png'
+            ],
+            [
+                'title' => 'монолитное',
+                'subtitle' => 'строительство',
+                'path_text' => null,
+                'img' => '/src/assets/3.png'
+            ],
+            [
+                'title' => 'шпунтовое',
+                'subtitle' => 'ограждение',
+                'path_text' => null,
+                'img' => '/src/assets/4.png'
+            ],
+            [
+                'title' => 'Буронабивные',
+                'subtitle' => 'Cваи',
+                'path_text' => null,
+                'img' => '/src/assets/5.png'
+            ],
+            [
+                'title' => 'Буроинъекционные',
+                'subtitle' => 'Cваи',
+                'path_text' => null,
+                'img' => '/src/assets/6.png'
+            ],
+            [
+                'title' => 'РИТ, ЭРСТ',
+                'subtitle' => 'Cваи',
+                'path_text' => null,
+                'img' => '/src/assets/7.png'
+            ],
+            [
+                'title' => 'гидро',
+                'subtitle' => 'изоляция',
+                'path_text' => null,
+                'img' => '/src/assets/8.png'
+            ],
+            [
+                'title' => 'Разработка',
+                'subtitle' => 'котлованов',
+                'path_text' => null,
+                'img' => '/src/assets/9.png'
+            ],
+            [
+                'title' => 'Укрепление',
+                'subtitle' => 'грунтов',
+                'path_text' => null,
+                'img' => '/src/assets/10.png'
+            ],
+        ];
+    }
 }
