ceace82fe64357993370f1876db264263938d210
blezz-tech <markus.jenya04@yandex.ru>
2024-08-31 22:24:23 +0300

    Интрегация боковой панели

--- frontend-v1/src/components/SideBar.vue
+++ frontend-v1/src/components/SideBar.vue
@@ -1,20 +1,32 @@
+<script>
+import { defineComponent, computed, ref } from 'vue'
+
+export default defineComponent({
+  name: "SideBar"
+})
+
+</script>
+
+<script setup>
+import { navData } from 'src/lib/data';
+
+const menu = ref(navData);
+
+</script>
+
 <template>
     <div class="bg-white rounded-borders shadow-24" style="margin-left: auto;">
         <h4 class="q-ma-xl">Все наши услуги</h4>
         <ul class="q-ma-xl">
-            <li><a href="">Генеральный подряд</a></li>
-            <li><a href="">Стена в грунте</a></li>
-            <li><a href="">Монолитное строительство</a></li>
-            <li><a href="">Разработка котлованов</a></li>
-            <li><a href="">Шпунтовое ограждение</a></li>
-            <li><a href="">Буроинъекционные сваи</a></li>
-            <li><a href="">Буронабивные сваи</a></li>
-            <li><a href="">Сваи РИТ, ЭРСТ, Микродур</a></li>
-            <li><a href="">Укрепление грунтов</a></li>
-            <li><a href="">Цементация</a></li>
-            <li><a href="">Усиление фундамента</a></li>
-            <li><a href="">Анкерные системы</a></li>
-            <li><a href="">Гидроизоляция</a></li>
+            <li
+                v-for="item in menu"
+                :key="item.id"
+            >
+                <router-link
+                    :to="{ name: 'Service', params: {id: item.file_id }}"
+                    style="text-decoration: none;"
+                >{{ item.full_name }}</router-link>
+            </li>
         </ul>
     </div>
 </template>
