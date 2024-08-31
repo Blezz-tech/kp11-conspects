48b6e469c390187e19702ac0fd96f3b4437d0fc4
blezz-tech <markus.jenya04@yandex.ru>
2024-08-31 22:13:00 +0300

    Вынос SideBar в отдельный модуль

---
+++ frontend-v1/src/components/SideBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-white rounded-borders shadow-24" style="margin-left: auto;">
+        <h4 class="q-ma-xl">Все наши услуги</h4>
+        <ul class="q-ma-xl">
+            <li><a href="">Генеральный подряд</a></li>
+            <li><a href="">Стена в грунте</a></li>
+            <li><a href="">Монолитное строительство</a></li>
+            <li><a href="">Разработка котлованов</a></li>
+            <li><a href="">Шпунтовое ограждение</a></li>
+            <li><a href="">Буроинъекционные сваи</a></li>
+            <li><a href="">Буронабивные сваи</a></li>
+            <li><a href="">Сваи РИТ, ЭРСТ, Микродур</a></li>
+            <li><a href="">Укрепление грунтов</a></li>
+            <li><a href="">Цементация</a></li>
+            <li><a href="">Усиление фундамента</a></li>
+            <li><a href="">Анкерные системы</a></li>
+            <li><a href="">Гидроизоляция</a></li>
+        </ul>
+    </div>
+</template>
--- frontend-v1/src/pages/IndexPage.vue
+++ frontend-v1/src/pages/IndexPage.vue
@@ -1,3 +1,24 @@
+<script setup>
+
+</script>
+
+
+
+<script>
+import {ref} from 'vue'
+import SideBar from 'src/components/SideBar.vue';
+
+
+export default {
+  components: { SideBar },
+  setup() {
+    return {
+      slide: ref(1)
+    }
+  }
+}
+</script>
+
 <template>
 
 
@@ -26,24 +47,8 @@
         </div>
       </div>
 
-      <div class="bg-white rounded-borders shadow-24" style="margin-left: auto;">
-        <h4 class="q-ma-xl">Все наши услуги</h4>
-        <ul class="q-ma-xl">
-          <li><a href="">Генеральный подряд</a></li>
-          <li><a href="">Стена в грунте</a></li>
-          <li><a href="">Монолитное строительство</a></li>
-          <li><a href="">Разработка котлованов</a></li>
-          <li><a href="">Шпунтовое ограждение</a></li>
-          <li><a href="">Буроинъекционные сваи</a></li>
-          <li><a href="">Буронабивные сваи</a></li>
-          <li><a href="">Сваи РИТ, ЭРСТ, Микродур</a></li>
-          <li><a href="">Укрепление грунтов</a></li>
-          <li><a href="">Цементация</a></li>
-          <li><a href="">Усиление фундамента</a></li>
-          <li><a href="">Анкерные системы</a></li>
-          <li><a href="">Гидроизоляция</a></li>
-        </ul>
-      </div>
+      <SideBar>
+      </SideBar>
     </div>
   </div>
   <!--________-->
@@ -133,21 +138,4 @@
 
 </template>
 
-<script setup>
 
-</script>
-
-
-<script>
-import {ref} from 'vue'
-
-export default {
-  setup() {
-    return {
-      slide: ref(1)
-    }
-  }
-}
-
-
-</script>
