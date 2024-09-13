37f83882941562b3fba2affbd352c5db0dc62d31
blezz-tech <markus.jenya04@yandex.ru>
2024-09-13 19:25:00 +0300

    Добавление SideBar для сервисов

--- frontend-v1/src/pages/services/ServiceAticle.vue
+++ frontend-v1/src/pages/services/ServiceAticle.vue
@@ -6,12 +6,24 @@ import { articles } from 'src/lib/data';
 function fakeFetch(id) {
   return articles.filter(item => item.id == id)[0];
 }
+</script>
 
 
+
+<script>
+import SideBar from 'src/components/SideBar.vue';
+
+export default {
+  components: { SideBar },
+}
 </script>
 
 <template>
-  <MarkdownRenderer
-    :source="fakeFetch(Number($route.params.id)).text"
-  />
+  <div class="narrow-block flex col-12 row items-start" style="margin-left: auto;">
+    <div class="flex column col-7">
+      <MarkdownRenderer :source="fakeFetch(Number($route.params.id)).text" />
+    </div>
+    <SideBar>
+    </SideBar>
+  </div>
 </template>
