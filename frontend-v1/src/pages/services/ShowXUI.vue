8fdb7647c1d3d8bfb42f0c233e2757eb6b9e1e73
blezz-tech <markus.jenya04@yandex.ru>
2024-09-13 19:48:12 +0300

    переименование

--- frontend-v1/src/pages/services/ShowXUI.vue
+++ frontend-v1/src/pages/services/ShowXUI.vue
@@ -1,7 +1,7 @@
 <script setup>
 import MarkdownRenderer from 'src/components/utils/MarkdownRenderer.vue';
 
-import { articles } from 'src/lib/data';
+import { articles } from 'src/fake_database/data';
 
 function fakeFetch(id) {
   return articles.filter(item => item.id == id)[0];
