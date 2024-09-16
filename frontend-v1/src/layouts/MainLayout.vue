dc40650a9e8ccbdaa8f0528ec37d122da5abbe2b
blezz-tech <markus.jenya04@yandex.ru>
2024-09-16 20:18:22 +0300

    Вынос слайдера с красивыми картинками

--- frontend-v1/src/layouts/MainLayout.vue
+++ frontend-v1/src/layouts/MainLayout.vue
@@ -1,32 +1,24 @@
 <script>
 import FooterPanel from 'src/components/panels/FooterPanel.vue';
 import HeaderHeader from 'src/components/panels/HeaderHeader.vue';
+import ImagesSlider from 'src/components/panels/ImagesSlider.vue';
 import NavMenu from 'src/components/panels/NavMenu.vue';
-import { ref } from 'vue'
 
 export default {
-  components: { HeaderHeader, NavMenu, FooterPanel },
-  setup() {
-    return {
-      slide: ref(1)
-    }
-  }
+  components: {
+    HeaderHeader,
+    NavMenu,
+    ImagesSlider,
+    FooterPanel,
+  },
 }
+
 </script>
 
 <template>
   <q-layout view="lHh lpR fff" class="m-bg">
     <HeaderHeader />
-    <div style="background-color: #212c32;">
-      <div class="narrow-block bg-white relative-position" style="top: 125px">
-        <q-carousel animated v-model="slide" arrows infinite>
-          <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
-          <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
-          <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
-          <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
-        </q-carousel>
-      </div>
-    </div>
+    <ImagesSlider />
     <NavMenu />
     <q-page-container class="m-bg" style="padding-top: 0">
       <q-page>
