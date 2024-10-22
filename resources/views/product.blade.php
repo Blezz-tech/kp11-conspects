080097865a77d1ac7f8aeb7abcb6e4ad896d22c1
blezz-tech <markus.jenya04@yandex.ru>
2024-10-22 16:24:20 +0300

    Исправление детальной страницы товара

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
     <div class="card w-60">
-        <img src="{{ asset($product->img_path) }}" class="card-img-top w-60" alt="...">
         <div class="card-body">
+            <img src="{{ asset($product->img_path) }}" class="card-img-top w-60" alt="Картинка рояля">
             <h5 class="card-title">{{ $product->title }}</h5>
-            <p class="card-text">{{ $product->price }}</p>
+            <p class="card-text">Стоимость: {{ $product->price }}</p>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">Год производства {{ $product->year }}</li>
-            <li class="list-group-item">Модель {{ $product->model }}</li>
-            <li class="list-group-item">Страна {{ $product->country }}</li>
+            <li class="list-group-item">Год производства: {{ $product->year }}</li>
+            <li class="list-group-item">Модель: {{ $product->model }}</li>
+            <li class="list-group-item">Страна: {{ $product->country }}</li>
         </ul>
         <div class="card-body">
             @auth
