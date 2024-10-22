6691863f7ace1b8008a8a9864bdb9ab5742003f1
blezz-tech <markus.jenya04@yandex.ru>
2024-10-22 16:07:41 +0300

    починка карусели

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,23 +1,37 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="{{ asset($products[0]->img_path) }}" class="d-block w-50" alt="нет">
-            <div class="carousel-caption d-none d-md-block">
-                <h5 style="color: #000">{{ $products[0]->title }}</h5>
-                <p style="color: #000">{{ $products[0]->price }}</p>
-            </div>
+    <div id="carouselExampleCaptions" class="carousel carousel-dark slide m-auto" style="width: 640px;">
+        <div class="carousel-indicators">
+            @foreach ($products as $i => $product)
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="{{ $i }}"
+                    aria-current="true" aria-label="Слайд 1" @class([
+                        'active' => $i == 0,
+                    ])>
+                </button>
+            @endforeach
         </div>
-
-        @for ($i = 1; $i < 5; $i++)
-            <div class="carousel-item">
-                <img src="{asset($products[$i]->img_path)}" class="d-block w-50" alt="нет">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5 style="color: #000">{{$products[$i]->title}}</h5>
-                    <p style="color: #000">{{$products[$i]->price}}</p>
+        <div class="carousel-inner">
+            @foreach ($products as $i => $product)
+                <div @class(['carousel-item', 'active' => $i == 0])>
+                    <img src="{{ asset($product->img_path) }}" class="d-block" style="width: 640px; height:480px;"
+                        alt="Картинка 1">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>{{$product->title}}</h5>
+                        <p>{{$product->price}}</p>
+                    </div>
                 </div>
-            </div>
-        @endfor
+            @endforeach
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Предыдущий слайд</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Следующий слайд</span>
+        </button>
     </div>
 @endsection
