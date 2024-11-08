a33a59a82220aca1ce07f719e6e5de15c31e2e7b
NoortZee <100483497+NoortZee@users.noreply.github.com>
2024-11-08 11:38:14 +0300

    пункт 3.
    Добавлена кнопка

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -7,7 +7,7 @@
 <table class="table table-striped mt-5">
     <thead>
         <tr>
-        <th scope="col">Дата</th>
+            <th scope="col">Дата</th>
             <th scope="col">ФИО</th>
             <th scope="col">Кол-во</th>
             <th scope="col">Статус</th>
@@ -15,22 +15,30 @@
         </tr>
     </thead>
     <tbody>
-@foreach ($orders as $order)
-    <tr>
-        <td scope="row">{{ $order->created_at }}</td>
-        <td>{{ $order->user->surname . ' ' . $order->user->name . ' ' . $order->user->patronymic }}</td>
-        <td>{{ $order->products_count }}</td>
-        <td>{{ $order->status }}</td>
-        <td>
-            @if ($order->status === 'отменен')
-                Причина отмены: {{ $order->coment }}
-            @else
-                Нет комментария
-            @endif
-        </td>
-    </tr>
-@endforeach
-</tbody>
+        @foreach ($orders as $order)
+            <tr>
+                <td scope="row">{{ $order->created_at }}</td>
+                <td>{{ $order->user->surname . ' ' . $order->user->name . ' ' . $order->user->patronymic }}</td>
+                <td>{{ $order->products_count }}</td>
+                <td>{{ $order->status }}</td>
+                <td>
+                    @if ($order->status === 'отменен')
+                        Причина отмены: {{ $order->coment }}
+                    @else
+                        Нет комментария
+                    @endif
+                </td>
+                <td>
+                    {{Sorder->status}}
+                    @if($order->status == 'новый')
+                        <a href="{{route('admin.orders.confirm', $order)}}" class="btn btn-primary">
+                            Подтвердить заказ
+                        </a>
+                    @endif
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
 </table>
 
 
