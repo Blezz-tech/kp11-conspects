171426c3eeffc5f438eff58af3a4f8ff63295cca
blezz-tech <markus.jenya04@yandex.ru>
2024-11-27 17:44:25 +0300

    ИСправление

--- 4 курс/Численные методы/ПР10.py
+++ 4 курс/Численные методы/ПР10.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import inspect
+
+os.makedirs("./data/ПР10", exist_ok=True)
 
 # Чистая функция для вычисления полинома Лагранжа
 def lagrange_interpolation(x, x_nodes, y_nodes):
@@ -13,7 +17,25 @@ def lagrange_interpolation(x, x_nodes, y_nodes):
     return L
 
 
-def resolver(x, a, f):
+def resolver(title, x, a, f, f_name):
+    print("\n## Задача", title)
+
+    # Условие
+    print("\n### Условие:")
+    print("\nПостроить интерполяционный полином Лагранжа для функции f (x) с узлами интерполирования xi, i = 0, 1, 2. Вычислить значения f (x) и полинома Лагранжа в точке a. Построить графики полинома Лагранжа и аппроксимируемой функции f (x) на отрезке [x0, x2].")
+    print(f"x = {x}")
+    print(f"a = {a}")
+    print()
+
+    # Решение
+    print("\n### Решение:")
+
+    with open('ПР10.py', 'r') as file:
+        content = file.read()
+        print("")
+        print("```")
+        print(content)
+        print("```")
 
     # Узлы интерполирования
     x_nodes = np.array(x)
@@ -31,8 +53,8 @@ def resolver(x, a, f):
 
     # Построение графиков
     plt.figure(figsize=(10, 6))
-    plt.plot(x_values, f_values, label='f(x) = (ln(x))^(13/4)', color='blue')
-    plt.plot(x_values, L_values, label='Полином Лагранжа', color='red', linestyle='--')
+    plt.plot(x_values, f_values, label=f_name, color='blue')
+    plt.plot(x_values, L_values, label='Полином Лагранжа', color='red', linestyle='dashed')
     plt.scatter(x_nodes, y_nodes, color='green', label='Узлы интерполирования')
     plt.scatter(a, f_a, color='orange', label=f'f({a}) = {f_a:.4f}', zorder=5)
     plt.scatter(a, L_a, color='purple', label=f'L({a}) = {L_a:.4f}', zorder=5)
@@ -41,18 +63,27 @@ def resolver(x, a, f):
     plt.ylabel('y')
     plt.axhline(0, color='black', linewidth=0.5, ls='--')
     plt.axvline(0, color='black', linewidth=0.5, ls='--')
+    plt.xlim(min(x), max(x))
     plt.legend()
     plt.grid()
-    plt.show()
+    # plt.show()
+    plt.savefig(f"./data/ПР10/task-{title}.jpg", dpi=300, bbox_inches='tight')
+    plt.clf()
 
     # Ответ
     print(f"f({a}) = {f_a:.4f}")
     print(f"L({a}) = {L_a:.4f}")
 
-x = [2, 3, 4]
+tasks = [
+    {
+        "title": "1",
+        "x": [2, 3, 4],
+        "a": 2.5,
+        "f": lambda x: (np.log(x))**(13/4)
+    }
+]
 
-a = 2.5
-
-f = lambda x:  (np.log(x))**(13/4)
-
-resolver(x, a, f)
+for task in tasks:
+    title, x, a, f = task['title'], task['x'], task['a'], task['f']
+    f_name = inspect.getsource(f).strip().split("lambda x: ")[1]
+    resolver(title, x, a, f, f_name)
