73bb424e32d1f187780ea64b7c9a85ce70a06fce
blezz-tech <markus.jenya04@yandex.ru>
2024-11-27 17:23:45 +0300

    Вынос функции

--- 4 курс/Численные методы/ПР10.py
+++ 4 курс/Численные методы/ПР10.py
@@ -13,42 +13,46 @@ def lagrange_interpolation(x, x_nodes, y_nodes):
     return L
 
 
-# Определяем функцию f(x)
-def f(x):
-    return (np.log(x))**(13/4)
+def resolver(x, a, f):
 
-# Узлы интерполирования
-x_nodes = np.array([2, 3, 4])
-y_nodes = f(x_nodes)
+    # Узлы интерполирования
+    x_nodes = np.array(x)
+    y_nodes = f(x_nodes)
 
-# Точка для вычисления
+
+    # Вычисляем значения
+    f_a = f(a)
+    L_a = lagrange_interpolation(a, x_nodes, y_nodes)
+
+    # Генерируем значения для графика
+    x_values = np.linspace(2, 4, 100)
+    f_values = f(x_values)
+    L_values = lagrange_interpolation(x_values, x_nodes, y_nodes)
+
+    # Построение графиков
+    plt.figure(figsize=(10, 6))
+    plt.plot(x_values, f_values, label='f(x) = (ln(x))^(13/4)', color='blue')
+    plt.plot(x_values, L_values, label='Полином Лагранжа', color='red', linestyle='--')
+    plt.scatter(x_nodes, y_nodes, color='green', label='Узлы интерполирования')
+    plt.scatter(a, f_a, color='orange', label=f'f({a}) = {f_a:.4f}', zorder=5)
+    plt.scatter(a, L_a, color='purple', label=f'L({a}) = {L_a:.4f}', zorder=5)
+    plt.title('Интерполяция Лагранжа')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.axhline(0, color='black', linewidth=0.5, ls='--')
+    plt.axvline(0, color='black', linewidth=0.5, ls='--')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    # Ответ
+    print(f"f({a}) = {f_a:.4f}")
+    print(f"L({a}) = {L_a:.4f}")
+
+x = [2, 3, 4]
+
 a = 2.5
 
-# Вычисляем значения
-f_a = f(a)
-L_a = lagrange_interpolation(a, x_nodes, y_nodes)
+f = lambda x:  (np.log(x))**(13/4)
 
-# Генерируем значения для графика
-x_values = np.linspace(2, 4, 100)
-f_values = f(x_values)
-L_values = lagrange_interpolation(x_values, x_nodes, y_nodes)
-
-# Построение графиков
-plt.figure(figsize=(10, 6))
-plt.plot(x_values, f_values, label='f(x) = (ln(x))^(13/4)', color='blue')
-plt.plot(x_values, L_values, label='Полином Лагранжа', color='red', linestyle='--')
-plt.scatter(x_nodes, y_nodes, color='green', label='Узлы интерполирования')
-plt.scatter(a, f_a, color='orange', label=f'f({a}) = {f_a:.4f}', zorder=5)
-plt.scatter(a, L_a, color='purple', label=f'L({a}) = {L_a:.4f}', zorder=5)
-plt.title('Интерполяция Лагранжа')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.axhline(0, color='black', linewidth=0.5, ls='--')
-plt.axvline(0, color='black', linewidth=0.5, ls='--')
-plt.legend()
-plt.grid()
-plt.show()
-
-# Ответ
-print(f"f({a}) = {f_a:.4f}")
-print(f"L({a}) = {L_a:.4f}")
+resolver(x, a, f)
