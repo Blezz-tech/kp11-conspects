564014754342151ad29356bfee1792c71492ad89
Blezz-tech <markus.jenya04@yandex.ru>
2024-12-20 11:13:59 +0300

    Доработка данных пользователя

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'login' => fake()->slug(1),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'is_accept' => true,
             'is_admin' => false,
             'remember_token' => Str::random(10),
         ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('login')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_accept');
             $table->boolean('is_admin');
             $table->rememberToken();
             $table->timestamps();
