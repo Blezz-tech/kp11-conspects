38b3db4c4727f13612252dda29859762a22a323b
Blezz-tech <markus.jenya04@yandex.ru>
2024-12-20 13:13:03 +0300

    Добавление ORM

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Ticket extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the categories that owns the comment.
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Get the post that owns the comment.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
 }
