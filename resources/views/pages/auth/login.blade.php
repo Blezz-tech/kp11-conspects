063bcb1358b3ecdb5ef034c01d3269cfcb78e655
Blezz-tech <markus.jenya04@yandex.ru>
2024-12-20 12:37:13 +0300

    Добавление TODO для форма

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -2,12 +2,16 @@
     <form action="{{ route('auth.login') }}" method="POST">
         @csrf
         <div class="mb-3">
-          <label for="login" class="form-label">Логин</label>
-          <input name="login" type="login" class="form-control" id="login" aria-describedby="login field" >
+            <label for="login" class="form-label">Логин</label>
+            <input name="login" type="login" class="form-control" id="login" aria-describedby="loginHelp" >
+            <div id="loginHelp" class="form-text">We'll never share your email with anyone else.</div>
+            {{-- TODO: Сделать подсветку ошибочных полей --}}
         </div>
         <div class="mb-3">
-          <label for="password" class="form-label">Пароль</label>
-          <input name="password" type="password" class="form-control" id="password">
+            <label for="password" class="form-label">Пароль</label>
+            <input name="password" type="password" class="form-control" id="passwordHelp">
+            <div id="passwordHelp" class="form-text">We'll never share your email with anyone else.</div>
+            {{-- TODO: Сделать подсветку ошибочных полей --}}
         </div>
         <button type="submit" class="btn btn-primary">Войти</button>
     </form>
