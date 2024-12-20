ced24f7c239aef93a5839b554ce3cde8507c8a92
Blezz-tech <markus.jenya04@yandex.ru>
2024-12-20 10:46:30 +0300

    Вынос layout'а в компонент

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/bootstrap.css">
-    <title>Document</title>
-</head>
-<body>
-    <x-navbar />
+<x-layout>
 
-    <script src="/js/bootstrap.bundle.js"></script>
-</body>
-</html>
+</x-layout>
