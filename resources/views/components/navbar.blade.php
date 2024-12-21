fcca9b8c117ff2a72b037a23ca3cd58f67521120
blezz-tech <markus.jenya04@yandex.ru>
2024-12-21 23:31:50 +0300

    Доработка навбара

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,9 +8,11 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Админка</a>
-                        </li>
+                        @admin
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ route('admin.panel') }}">Админка</a>
+                            </li>
+                        @endadmin
                         @auth
                             <li class="nav-item">
                                 <a class="nav-link active" href="{{ route('user.account') }}">Личный кабинет</a>
