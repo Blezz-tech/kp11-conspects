1202544fa00fc1b88b0cfefa33f719f37cebc833
blezz-tech <markus.jenya04@yandex.ru>
2024-12-22 01:07:42 +0300

    FIX: TicketFactory repaired

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\State;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,10 +19,17 @@ class TicketFactory extends Factory
      */
     public function definition(): array
     {
+        $state = State::inRandomOrder()->first()->id;
         return [
-            'description' => fake()->text(),
             'title' => fake()->text(),
-            'comment' => fake()->text()
+            'description' => fake()->text(),
+            'photo_before' => '-',
+            'photo_after' => $state == 1 ? '-' : null,
+            'comment' => $state == 1 ? fake()->text() : null,
+
+            'user_id' => User::inRandomOrder()->first()->id,
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'state_id' => $state,
         ];
     }
 }
