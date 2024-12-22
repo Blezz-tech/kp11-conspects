d61cc1502ec0d7917dc355017a6c10529b86a727
blezz-tech <markus.jenya04@yandex.ru>
2024-12-22 19:16:12 +0300

    Смена услуг для фабрики

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,6 +14,7 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         Category::create(['name' => 'услуга']);
-        Category::create(['name' => 'дичь']);
+        Category::create(['name' => 'ремонт дорог']);
+        Category::create(['name' => 'уборка мусора']);
     }
 }
