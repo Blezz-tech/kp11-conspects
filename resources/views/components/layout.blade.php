0e84c4c95d04985239a107c434d45cc79ff95451
Blezz-tech <markus.jenya04@yandex.ru>
2024-12-23 11:02:27 +0300

    Добавлне layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+
+    <link rel="icon" type="image/x-icon" href="{{asset('logo.png')}}">
+    <title>Портал</title>
+</head>
+<body>
+    <x-navbar />
+    <div class="container mt-3">
+        {{ $slot }}
+    </div>
+    <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
+</body>
+</html>
