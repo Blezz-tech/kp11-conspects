961b3e80cca88aa96032b6e925c916f29c19913e
Blezz-tech <markus.jenya04@yandex.ru>
2024-12-23 11:41:08 +0300

    ++

---
+++ resources/views/admin/tickets/index.blade.php
@@ -0,0 +1,71 @@
+<x-layout-errors>
+    <form action="{{ route('admin.tickets.index') }}" method="GET">
+        @csrf
+        <div class="mb-3">
+            <select name="state_id" class="form-select" aria-label="Default select example">
+                <option value="" selected>все</option>
+                @foreach ($states as $state)
+                    <option value="{{ $state->id }}">{{ $state->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Фильтровать</button>
+    </form>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Время</th>
+            <th scope="col">Название</th>
+            <th scope="col">Описание</th>
+            <th scope="col">Категория</th>
+            <th scope="col">Статус</th>
+            <th scope="col">До</th>
+            <th scope="col">После</th>
+            <th scope="col">Коммент</th>
+            <th scope="col">Действие</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($tickets as $ticket)
+                <tr>
+                    <th scope="row">{{ $ticket->id }}</th>
+                    <td>{{ $ticket->created_at }}</td>
+                    <td>{{ $ticket->s_title }}</td>
+                    <td>{{ $ticket->s_description }}</td>
+                    <td>{{ $ticket->category->name }}</td>
+                    <td>
+                        <div @class([
+                            'text-center' => true,
+                            'alert' => true,
+                            'alert-primary' => $ticket->state->id == 1,
+                            'alert-success' => $ticket->state->id == 2,
+                            'alert-danger' => $ticket->state->id == 3,
+                        ]) role="alert">
+                            {{ $ticket->state->name }}
+                        </div>
+                    </td>
+                    <td>
+                        <img style="max-width: 100px; height: 75px;" src="{{ asset($ticket->photo_before) }}" alt="Photo Before" />
+                    </td>
+                    <td>
+                        @if ($ticket->photo_after != null)
+                            <img style="max-width: 100px; height: 75px;" src="{{ asset($ticket->photo_after) }}" alt="Photo Before" />
+                        @else
+                            -
+                        @endif
+                    </td>
+                    <td>{{ $ticket->s_comment }}</td>
+                    <td>
+                        @if ($ticket->state->id == 1)
+                            <div class="d-flex flex-column gap-1">
+                                <a class="btn btn-outline-primary" href="{{ route('admin.tickets.acceptPage', $ticket) }}" role="button">Принять</a>
+                                <a class="btn btn-outline-danger" href="{{ route('admin.tickets.rejectPage', $ticket) }}" role="button">Отклонить</a>
+                            </div>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</x-layout-errors>
