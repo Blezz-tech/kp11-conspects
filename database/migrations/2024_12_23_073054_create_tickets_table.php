fcef948c02e37f1961829572cba3139ffd8fb6ff
Blezz-tech <markus.jenya04@yandex.ru>
2024-12-23 10:38:59 +0300

    Доработка миграций

--- database/migrations/2024_12_23_073054_create_tickets_table.php
+++ database/migrations/2024_12_23_073054_create_tickets_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->date('date_get');
+            $table->bool('is_nalink');
             $table->timestamps();
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('state_id')->constrained()->onDelete('cascade')->default(0);
         });
     }
 
