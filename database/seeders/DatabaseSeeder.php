7c0f20f376ef2812e76db795fe8c0631a62a0c95
blezz-tech <markus.jenya04@yandex.ru>
2025-03-03 18:46:30 +0300

    add service seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Service;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([ // здесь регистрируем все сидоры, которые у нас есть, чтобы запускать их с помощью команды php artisan db:seed
             UserSeeder::class,
+            ServiceSeeder::class,
         ]);
     }
 }
