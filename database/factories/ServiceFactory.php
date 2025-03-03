3314129d03d3b529b9220482b498ea3665e1168d
blezz-tech <markus.jenya04@yandex.ru>
2025-03-03 19:23:20 +0300

    fix

--- database/factories/ServiceFactory.php
+++ database/factories/ServiceFactory.php
@@ -17,7 +17,7 @@ class ServiceFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->text(12),
             'price' => fake()->numberBetween(1000, 15000),
             'description' => fake()->text(200),
         ];
